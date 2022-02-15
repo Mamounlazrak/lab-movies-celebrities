b4a52c4c890b2753cc21c67a616722b681bd71e1
Mamoun Lazrak <mamounlazrak@gmail.com>
2022-02-15 18:34:47 +0000

    lab done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const Movie = require('../models/Movie.model');
 const Celebrity = require('../models/Celebrity.model');
+const { route } = require('.');
 
 router.get('/movies/create', (req, res, next) => {
     Celebrity.find()
@@ -17,10 +18,61 @@ router.post('/movies/create', (req, res, next) => {
     const {title, genre, plot, cast} = req.body;
     Movie.create({title, genre, plot, cast})
         .then((createdMovie) => {
-            res.redirect('/movies/movies')
+            res.redirect('/movies')
         })
         .catch((err) => console.log(err));
 
+})
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then((foundMovies) => {
+            res.render('movies/movies', {movies: foundMovies})
+        })
+        .catch((err) => console.log(err));
+})
+
+router.get('/movies/:id', (req, res, next) => {
+    const {id} = req.params; 
+    Movie.findById(id)
+        .populate('cast')
+        .then((foundMovie) => {
+            res.render('movies/movie-details', foundMovie)
+        })
+        .catch((err) => console.log(err))
+})
+
+router.post('/movies/:id/delete', (req, res, next) => {
+    const {id} = req.params; 
+    Movie.findByIdAndDelete(id)
+        .then(() => {
+            res.redirect('/movies')
+        })
+        .catch((err) => console.log(err));
+})
+
+router.get('/movies/:id/edit', (req, res, next) => {
+    const {id} = req.params; 
+    let movie; 
+    Movie.findById(id)
+        .then((foundMovie) => {
+            movie = foundMovie;
+            return Celebrity.find()
+        })
+        .then((foundCelebrities) => {
+
+            res.render('movies/edit-movie', {movie, foundCelebrities} )
+        })
+        .catch((err) => console.log(err)); 
+})
+
+router.post('/movies/:id/edit', (req, res, next) => {
+    const {id} = req.params;
+    const {title, genre, plot, cast} = req.body;
+    Movie.findByIdAndUpdate(id, {title, genre, plot, cast})
+        .then(() => {
+            res.redirect('/movies')
+        })
+        .catch((err) => console.log(err));
 })
 
 
